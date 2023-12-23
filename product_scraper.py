766f7896980433e04bc95ca278fc4b5e577bfa56
Mohamed Hafeel <lkostyrka17@gmail.com>
2023-12-23 10:07:34 -0500

    Fix: Tesco block

--- product_scraper.py
+++ product_scraper.py
@@ -4,6 +4,7 @@ import sys
 import time
 import math
 import pandas
+import random
 import logging
 import logging.handlers
 from typing import List
@@ -32,6 +33,7 @@ def get_product_details(links: List[str], sbr_connection: ChromiumRemoteConnecti
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--start-maximized")
     for link in links:
+        time.delay(random.choice([0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]))
         try:
             with Remote(sbr_connection, options=chrome_options) as driver:
                 driver.get(link)
